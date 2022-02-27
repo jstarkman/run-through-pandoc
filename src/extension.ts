8ec9332100e2528fb7729ccf2ba9e6c16a306ec2
James Starkman <james@starkman.net>
2022-02-27 02:56:51 -0500

    Added feature to prompt for input and output formats.

--- src/extension.ts
+++ src/extension.ts
@@ -3,15 +3,22 @@
 import * as vscode from 'vscode';
 import * as child_process from 'child_process';
 
+var supportedInputFormats: string[] = [];
+var supportedOutputFormats: string[] = [];
+
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
+	initSupportedFormats();
+	console.log('This Pandoc supports:', supportedInputFormats, supportedOutputFormats);
 	// FIXME ensure that Pandoc is installed and on the PATH; or config for path
 
 	let disposables = [
 		vscode.commands.registerCommand('run-through-pandoc.markdownToJira', () => replaceActiveRegion('markdown', 'jira')),
 		vscode.commands.registerCommand('run-through-pandoc.markdownToMediawiki', () => replaceActiveRegion('markdown', 'mediawiki')),
 		vscode.commands.registerCommand('run-through-pandoc.markdownToTextile', () => replaceActiveRegion('markdown', 'textile')),
+		vscode.commands.registerCommand('run-through-pandoc.prompt', prompt),
+		vscode.commands.registerCommand('run-through-pandoc.promptFromMarkdown', promptFromMarkdown),
 	];
 
 	for (var disp in disposables) {
@@ -20,7 +27,25 @@ export function activate(context: vscode.ExtensionContext) {
 	}
 }
 
+async function prompt() {
+	const formatIn = await vscode.window.showQuickPick(supportedInputFormats, {
+		title: 'Select input format',
+	});
+	const formatOut = await vscode.window.showQuickPick(supportedOutputFormats, {
+		title: 'Select output format',
+	});
+	replaceActiveRegion(formatIn || 'markdown', formatOut || 'html');
+}
+
+async function promptFromMarkdown() {
+	const formatOut = await vscode.window.showQuickPick(supportedOutputFormats, {
+		title: 'Select output format',
+	});
+	replaceActiveRegion('markdown', formatOut || 'html');
+}
+
 function replaceActiveRegion(formatFrom: string, formatTo: string) {
+	console.debug(`Converting from ${formatFrom} to ${formatTo}.`)
 	const editor = vscode.window.activeTextEditor;
 	if (!editor) return;
 	const document = editor.document;
@@ -39,10 +64,23 @@ function pandoc(formatFrom: string, formatTo: string, input: string): string {
 	], () => input);
 }
 
-function shellOut(cmd: string, args: string[], toStdin: (() => string)) {
+function initSupportedFormats() {
+	const tx = shellOut('pandoc', ['--list-input-formats'])
+		.split("\n")
+		.map(s => s && s.trim() || '')
+		.filter(x => !!x);
+	supportedInputFormats = tx;
+	const rx = shellOut('pandoc', ['--list-output-formats'])
+		.split("\n")
+		.map(s => s && s.trim() || '')
+		.filter(x => !!x);
+	supportedOutputFormats = rx;
+}
+
+function shellOut(cmd: string, args: string[], toStdin?: (() => string)) {
 	const command = cmd + ' ' + args.join(' ');
 	const stdout: string = child_process.execSync(command, {
-		input: toStdin(),
+		input: toStdin && toStdin() || '',
 		timeout: 1000,  // arbitrary but finite
 		encoding: 'utf-8',
 	});
